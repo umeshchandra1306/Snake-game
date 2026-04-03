f59909fa6cd8709aabe7420e0e40fa98a8393d54
rk0843632-del <rk0843632@gmail.com>
2026-04-03 16:29:37 +0530

    Added main.c file

---
+++ main.c
@@ -0,0 +1,22 @@
+#include "snake.h"
+
+GameState gs;
+GtkWidget *canvas;
+guint timer_id = 0;
+
+int main(int argc, char *argv[])
+{
+
+    srand((unsigned)time(NULL));
+    gtk_init(&argc, &argv);
+
+    GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+
+    gtk_window_set_title(GTK_WINDOW(window), "Snake Game — GTK3");
+
+    gtk_window_set_default_size(GTK_WINDOW(window), WINDOW_W, WINDOW_H);
+
+    gtk_window_set_resizable(GTK_WINDOW(window), FALSE);
+
+    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+}
